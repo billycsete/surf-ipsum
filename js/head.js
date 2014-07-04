a43d5b2a61e03ea6b0417dd9762404c25f5a73d2
wscete <wcsete@gmail.com>
2014-07-03 22:48:21 -0700

    added initial project files

---
+++ js/head.js
@@ -0,0 +1,8 @@
+//============================================================
+//
+// HEAD
+//
+//============================================================
+
+// @codekit-prepend "../bower_components/html5shiv/dist/html5shiv.js"
+// @codekit-prepend "../bower_components/respond/src/respond.js"
