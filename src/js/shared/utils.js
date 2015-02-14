12e0990e1dd7403f2eefb5ac7a2dc46bd5be822a
wscete <wcsete@gmail.com>
2015-02-13 20:57:41 -0800

    added utils

---
+++ src/js/shared/utils.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * based on from https://github.com/inuyaksa/jquery.nicescroll/blob/master/jquery.nicescroll.js
+ */
+var hasParent = function( e, p ) {
+	if (!e) return false;
+	var el = e.target||e.srcElement||e||false;
+	while (el && el != p) {
+		el = el.parentNode||false;
+	}
+	return (el!==false);
+};
+
+module.exports.hasParent = hasParent;
