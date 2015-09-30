63572430ad0c4303b208605953ba9ef451cea377
wscete <wcsete@gmail.com>
2015-09-29 20:48:14 -0700

    reorganized the output generating methods

--- src/js/generator/IpsumOutput.js
+++ src/js/generator/IpsumOutput.js
@@ -28,7 +28,7 @@ proto = IpsumOutput.prototype;
  * Generate a random sentence
  * @return {String} - random sentence of surf words
  */
-proto.generateSentence = function ( ) {
+proto.generateSentence = function( ) {
 	var sentenceLength = Utils.getRandomInt( 5, 10 );
 	// get strings from the firebase database
 	var sentence = this.firebaseObject.getRandomStrings( sentenceLength );
@@ -43,23 +43,109 @@ proto.generateSentence = function ( ) {
 
 
 /**
+ * Generate random words
+ * @param {Number} numberOfWords - number of words to generate
+ * @param {String} - string of words
+ */
+proto.generateWords = function( numberOfWords ) {
+	// get strings from the firebase database
+	var words = this.firebaseObject.getRandomStrings( numberOfWords );
+	// turn array of results into one long string and remove commas
+	words = this._stringifyIpsumResults( words );
+	// print words to the output element
+	return words;
+};
+
+
+
+/**
+ * Generate a new paragraph element
+ * @return {Element} - <p>
+ */
+proto.generateParagraphElement = function( ) {
+	var paragraph = '';
+	var sentencesPerParagraph = Utils.getRandomInt( 5, 8 );
+
+	for ( var j = 0; j < sentencesPerParagraph; j++ ) {
+		paragraph += this.generateSentence() + ' ';
+	}
+
+	// trim off the trailing space on the last sentence
+	paragraph = paragraph.slice( 0, - 1 );
+
+	return $( '<p>' + paragraph + '</p>' );
+}
+
+
+
+/**
+ * Generate a new list element
+ * @return {Element} - <ul>
+ */
+proto.generateListElement = function( ) {
+	var listLength = Utils.getRandomInt( 4, 8 );
+	var listElement = document.createElement( 'ul' );
+
+	for (var j = 0; j < listLength; j++) {
+		var listItem = document.createElement( 'li' );
+
+		var listItemTextLength = Utils.getRandomInt( 2, 4 );
+		// get strings from the firebase database
+		var listItemText = this.firebaseObject.getRandomStrings( listItemTextLength );
+		// turn array of results into one long string and remove commas
+		listItemText = this._stringifyIpsumResults( listItemText );
+		// add the random text to the new list item
+		$(listItem).html( listItemText );
+		// add the new list item element to the list element
+		$(listElement).append( listItem );
+	}
+
+	return $( listElement );
+}
+
+
+
+/**
+ * Generate a new headline element
+ * @return {Element} - <h2>
+ */
+proto.generateHeadlineElement = function( ) {
+	var headlineLength = Utils.getRandomInt( 2, 4 );
+	// get strings from the firebase database
+	console.log(this.firebaseObject);
+	var headline = this.firebaseObject.getRandomStrings( headlineLength );
+	// turn array of results into one long string and remove commas
+	headline = this._stringifyIpsumResults( headline );
+	// capitalize headline
+	headline = this._capitalizeString( headline );
+
+	return ( '<h2>' + headline + this._generatePunctuationEnding() + '</h2>' );
+}
+
+
+
+/**
+ * Generate a paragraph block of words
+ * @param {Number} numberOfWords - number of words to add to the paragraph
+ * @return {Element} - <p>
+ */
+proto.generateWordBlockElement = function( numberOfWords ) {
+	var words = this.generateWords( numberOfWords );
+	return $( '<p>' + words + '</p>' );
+}
+
+
+
+/**
  * Print paragraphs to the output element
  * @param {Number} numberOfParagraphs - number of paragraphs to generate
  */
-proto.printParagraphsToOutputElement = function ( numberOfParagraphs ) {
-
+proto.printParagraphsToOutputElement = function( numberOfParagraphs ) {
 	for (var i = 0; i < numberOfParagraphs; i++) {
-		var sentencesPerParagraph = Utils.getRandomInt( 5, 8 );
-		var paragraph = '';
-
-		for ( var j = 0; j < sentencesPerParagraph; j++ ) {
-			paragraph += this.generateSentence() + ' ';
-		}
-
-		// trim off the trailing space on the last sentence
-		paragraph = paragraph.slice( 0, - 1 );
-		// print paragraph to the output element
-		this.$outputElement.prepend( '<p class="ipsum-item ipsum-item-paragraph"">' + paragraph + '</p>' );
+		// generate a new paragraph element
+		var paragraphElement = this.generateParagraphElement();
+		// prepend the new paragraph to the output element
+		this.$outputElement.prepend( paragraphElement );
 	}
 };
 
@@ -69,18 +155,12 @@ proto.printParagraphsToOutputElement = function ( numberOfParagraphs ) {
  * Print headlines to the output element
  * @param {Number} numberOfHeadlines - number of headlines to generate
  */
-proto.printHeadlinesToOutputElement = function ( numberOfHeadlines ) {
-
+proto.printHeadlinesToOutputElement = function( numberOfHeadlines ) {
 	for ( var i = 0; i < numberOfHeadlines; i++ ) {
-		var headlineLength = Utils.getRandomInt( 2, 4 );
-		// get strings from the firebase database
-		var headline = this.firebaseObject.getRandomStrings( headlineLength );
-		// turn array of results into one long string and remove commas
-		headline = this._stringifyIpsumResults( headline );
-		// capitalize headline
-		headline = this._capitalizeString( headline );
-		// print a new headline to the output element
-		this.$outputElement.prepend( '<h2 class="ipsum-item ipsum-item-headline">' + headline + this._generatePunctuationEnding() + '</h2>' );
+		// generate a new headline element
+		var headlineElement = this.generateHeadlineElement();
+		// prepend the new headline to the output element
+		this.$outputElement.prepend( headlineElement );
 	}
 };
 
@@ -90,27 +170,11 @@ proto.printHeadlinesToOutputElement = function ( numberOfHeadlines ) {
  * Print lists to the output element
  * @param {Number} numberOfLists - number of lists to generate
  */
-proto.printListsToOutputElement = function ( numberOfLists ) {
-
+proto.printListsToOutputElement = function( numberOfLists ) {
 	for ( var i = 0; i < numberOfLists; i++ ) {
-		var listLength = Utils.getRandomInt( 4, 8 );
-		var listElement = document.createElement( 'ul' );
-		$(listElement).addClass('ipsum-item ipsum-item-list');
-
-		for (var j = 0; j < listLength; j++) {
-			var listItem = document.createElement( 'li' );
-
-			var listItemTextLength = Utils.getRandomInt( 2, 4 );
-			// get strings from the firebase database
-			var listItemText = this.firebaseObject.getRandomStrings( listItemTextLength );
-			// turn array of results into one long string and remove commas
-			listItemText = this._stringifyIpsumResults( listItemText );
-			// add the random text to the new list item
-			$(listItem).html( listItemText );
-			// add the new list item element to the list element
-			$(listElement).append( listItem );
-		}
-
+		// generate a new list element
+		var listElement = this.generateListElement();
+		// prepend the new list to the output element
 		this.$outputElement.prepend( listElement );
 	}
 };
@@ -121,13 +185,9 @@ proto.printListsToOutputElement = function ( numberOfLists ) {
  * Print lists to the output element
  * @param {Number} numberOfWords - number of lists to generate
  */
-proto.printWordsToOutputElement = function ( numberOfWords ) {
-	// get strings from the firebase database
-	var words = this.firebaseObject.getRandomStrings( numberOfWords );
-	// turn array of results into one long string and remove commas
-	words = this._stringifyIpsumResults( words );
-	// print words to the output element
-	this.$outputElement.prepend( '<p class="ipsum-item ipsum-item-words">' + words + '</p>' );
+proto.printWordsToOutputElement = function( numberOfWords ) {
+	var wordBlockElement = this.generateWordBlockElement( numberOfWords );
+	this.$outputElement.prepend( wordBlockElement );
 };
 
 
