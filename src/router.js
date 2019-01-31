be24bcf6ddbd9da7ae7190a48090b34a39a152fb
Billy Csete <billycsete@gmail.com>
2019-01-31 11:47:48 -0800

    feat(): add router logic for uploader/login views

--- src/router.js
+++ src/router.js
@@ -33,14 +33,12 @@ let router = new Router({
 
 
 router.beforeEach((to, from, next) => {
-  console.log(to, from, next);
   let currentUser = firebase.auth().currentUser;
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
-  // if (requiresAuth && !currentUser) next('login');
-  // else if (!requiresAuth && currentUser) next('comics');
-  // else next();
-  next();
+  if (requiresAuth && !currentUser) next('login');
+  else if (!requiresAuth && currentUser) next('uploader');
+  else next();
 });
 
 export default router;
